178ce7fe3446835bb589d439d6ff8112d6c49322
redaelgountari <redaelgountari7@gmail.com>
2026-03-03 17:38:37 +0000

    update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,35 +1,13 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Register from '../GenComponents/Register'
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/components/ui/tabs"
 import Login from '../GenComponents/Login'
-import Link from "next/link";
 
 export default function page() {
   return (
     <div className='w-full max-w-md mx-auto'>
-       {/* <Tabs defaultValue="account" className="w-[400px]">
-      <TabsList className="grid w-full grid-cols-2">
-        <Link href="/signup">
-          <TabsTrigger value="Signup" asChild>
-            <span>Signup</span>
-          </TabsTrigger>
-        </Link>
-        <TabsTrigger value="password">Password</TabsTrigger>
-      </TabsList>
-      <TabsContent value="account">
-      </TabsContent>
-      <TabsContent value="password">
-      <Login/>
- 
-      </TabsContent>
-    </Tabs> */}
-    <Login/>
-        
+      <Suspense fallback={<div className="flex items-center justify-center p-8">Loading...</div>}>
+        <Login />
+      </Suspense>
     </div>
   )
 }
